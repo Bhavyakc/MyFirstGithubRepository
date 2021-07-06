636400eaa7a23717e0be4739f5fc812561a3d715
Bhavya <bhavyakc08@gmail.com>
2021-07-06 12:07:15 +0530

    Adding more C files

---
+++ 1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("");
+    printf("Adding more lines in code for git");
+     return 0;
+}
---
+++ index.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Bhavya ");
+     return 0;
+}
